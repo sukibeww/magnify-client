fe0e707b9b25f466a434ad0df3cdea79e4a6ff7d
sukibeww <sukialiong@gmail.com>
2019-12-13 18:52:45 +1100

    Landing page boilerplate implementation
    added some landing page relevant assets locally, created 3 components for the landing page.
    1. Landing ( higher level component )
    2. AccordionLanding ( child component of Landing component )
    3. AccordionLandingItem (child component of AccordionLanding component )
    
    😆

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components';
+import AccordionLanding from '../AccordionLanding/AccodionLanding'
+
+
+const LandingWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: row;
+  background-image: url(${require("./images/desktop-bg.png")});
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-height: 95vh;
+  width: 100%;
+`
+
+const StyledLogo = styled.img`
+  width: 35vw;
+  height: 35vw;
+`
+
+const Landing = () => {
+  return(
+    <>
+      <LandingWrapper>
+        <StyledLogo src={require('./images/landing-logo.png')} alt="magnifylogo"/>
+        <AccordionLanding />
+      </LandingWrapper>
+    </>
+  )
+}
+
+export default Landing;
