0f48280b49dc9f8a9a7f665405449024c67908b7
sukibeww <sukialiong@gmail.com>
2020-01-15 16:33:13 +1100

    delegates backend integration without result logic

--- src/components/Employer/Delegates/Delegates.js
+++ src/components/Employer/Delegates/Delegates.js
@@ -1,7 +1,8 @@
-import React , { useState } from 'react';
+import React , { useState, useContext, useEffect } from 'react';
 import MaterialTable from 'material-table';
 import styled from 'styled-components'
 import GeneralButton from '../../Button/GeneralButton';
+import { EmployerContext } from '../../../context/employerContext';
 
 const StyledWrapper = styled.div`
   border: solid #283593;
@@ -19,29 +20,43 @@ const StyledWrapper = styled.div`
 `
 
 const Delegates = () => {
+  const employerContext = useContext(EmployerContext)
+  const { getDelegates } = employerContext
   const [selected, setSelected] = useState([])
-  const [data, setData] = useState({
-    columns: [
-      { title: 'Name', field: 'name' },
-      { title: 'Email', field: 'email' },
-      { title: 'Rating', field: 'rating' }
-    ],
-    data: [
-      { name: 'Jimmy', email: 'jimmyjimmy@gmail.com', rating: "70%" },
-      { name: 'Michael', email: 'mich.my@gmail.com', rating: "59%" },
-      { name: 'Daniel', email: 'daniel.dan@gmail.com', rating: "44%" },
-      { name: 'Sam', email: 'samuel.sam@gmail.com', rating: "33%" },
-    ],
-  });
+  const [data, setData] = useState([
+      // { name: 'Jimmy', email: 'jimmyjimmy@gmail.com', rating: "70%" },
+      // { name: 'Michael', email: 'mich.my@gmail.com', rating: "59%" },
+      // { name: 'Daniel', email: 'daniel.dan@gmail.com', rating: "44%" },
+      // { name: 'Sam', email: 'samuel.sam@gmail.com', rating: "33%" },
+    ]);
+  const columns = [
+    { title: 'Name', field: 'displayName' ,filtering: false},
+    { title: 'Email', field: 'email',filtering: false },
+    { title: 'Rating', field: 'rating' ,filtering: false},
+    { title: 'Industry', field: 'category'},
+  ]
+  useEffect(() => {
+    const fetchDelegates = async() => {
+      const delegates = await getDelegates("Computer")
+      const optimisedDelegates = delegates.map((delegate) => {
+        delegate.category = delegate.category.join()
+        return delegate
+      })
+      console.log(optimisedDelegates)
+      setData(optimisedDelegates)
+    }
+    fetchDelegates()
+  }, [getDelegates])
 
   return (
     <StyledWrapper>
       <MaterialTable
         title="Delegates"
-        columns={data.columns}
-        data={data.data}
+        columns={columns}
+        data={data}
         options={{
-          selection: true
+          selection: true,
+          filtering: true
         }}
         onSelectionChange={(rows) => setSelected(rows)}
       />
