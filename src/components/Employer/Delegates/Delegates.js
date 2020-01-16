de73a034f757d8a5493162674dc2b1b7e8768135
sukibeww <sukialiong@gmail.com>
2020-01-16 15:48:48 +1100

    responsive fix

--- src/components/Employer/Delegates/Delegates.js
+++ src/components/Employer/Delegates/Delegates.js
@@ -6,35 +6,26 @@ import { EmployerContext } from '../../../context/employerContext';
 
 const StyledWrapper = styled.div`
   border: solid #283593;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-end;
   margin: 5vh auto;
   border-radius: 10px;
-  height: selectedauto;
-  width: 70%;
-  min-width: 400px;
   max-width: max-content;
   padding: 5vh 3vw;
+  margin: 3vw;
+  overflow: scroll;
 `
 
 const Delegates = () => {
   const employerContext = useContext(EmployerContext)
-  const { getDelegates } = employerContext
+  const { user, getDelegates } = employerContext
   const [selected, setSelected] = useState([])
-  const [data, setData] = useState([
-      // { name: 'Jimmy', email: 'jimmyjimmy@gmail.com', rating: "70%" },
-      // { name: 'Michael', email: 'mich.my@gmail.com', rating: "59%" },
-      // { name: 'Daniel', email: 'daniel.dan@gmail.com', rating: "44%" },
-      // { name: 'Sam', email: 'samuel.sam@gmail.com', rating: "33%" },
-    ]);
+  const [data, setData] = useState([]);
   const columns = [
     { title: 'Name', field: 'displayName' ,filtering: false},
     { title: 'Email', field: 'email',filtering: false },
     { title: 'Rating', field: 'rating' ,filtering: false},
     { title: 'Industry', field: 'category'},
   ]
+  console.log(user)
   useEffect(() => {
     const fetchDelegates = async() => {
       const delegates = await getDelegates("Computer")
@@ -49,20 +40,21 @@ const Delegates = () => {
   }, [getDelegates])
 
   return (
-    <StyledWrapper>
-      <MaterialTable
-        title="Delegates"
-        columns={columns}
-        data={data}
-        options={{
-          selection: true,
-          filtering: true
-        }}
-        onSelectionChange={(rows) => setSelected(rows)}
-      />
-      <GeneralButton label="Invite"/>
-    </StyledWrapper>
-    
+    <>
+      <StyledWrapper>
+        <MaterialTable
+          title="Delegates"
+          columns={columns}
+          data={data}
+          options={{
+            selection: true,
+            filtering: true
+          }}
+          onSelectionChange={(rows) => setSelected(rows)}
+        />
+        <GeneralButton label="Invite"/>
+      </StyledWrapper>
+    </>
   );
 }
 
