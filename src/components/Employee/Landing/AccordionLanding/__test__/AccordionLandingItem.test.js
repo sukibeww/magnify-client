e8f267227b4fe20972a3cd22a988b5acf1aa89e0
sukibeww <sukialiong@gmail.com>
2020-01-20 00:25:48 +1100

    testing clean up

--- src/components/Employee/Landing/AccordionLanding/__test__/AccordionLandingItem.test.js
+++ src/components/Employee/Landing/AccordionLanding/__test__/AccordionLandingItem.test.js
@@ -2,23 +2,35 @@ import React from 'react';
 import { render, cleanup, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import AccordionLandingItem from '../AccodionLandingItem';
-import { get } from 'http';
+import { MediaContext } from '../../../../../context/mediaContext';
 
 afterEach(cleanup)
 
 test('it renders main accodion item component', () => {
-  const { getByTestId } = render(<AccordionLandingItem header="header-test" summary="summary-test" initialState={false}/>);
+  const { getByTestId } = render(
+    <MediaContext.Provider value={{media: true}}>
+      <AccordionLandingItem header="header-test" summary="summary-test" initialState={false}/>
+    </MediaContext.Provider>
+  );
   const accordionItemWrapper = getByTestId('accordion-item-wrapper');
   expect(accordionItemWrapper.children.length).toBe(2);  
 });
 
 test('renders the header properly', () => {
-  const { getByText } = render(<AccordionLandingItem header="header-test" summary="summary-test" initialState={false} />);
+  const { getByText } = render(
+    <MediaContext.Provider value={{media: true}}>
+      <AccordionLandingItem header="header-test" summary="summary-test" initialState={false}/>
+    </MediaContext.Provider>
+  );
   expect(getByText("header-test")).toBeInTheDocument();
 });
 
 test('renders the summary properly for false initial state', () => {
-  const { getByTestId } = render(<AccordionLandingItem header="header-test" summary="summary-test" initialState={false}/>);
+  const { getByTestId } = render(
+    <MediaContext.Provider value={{media: true}}>
+      <AccordionLandingItem header="header-test" summary="summary-test" initialState={false}/>
+    </MediaContext.Provider>
+  );
   const accordionSummary = getByTestId("accordion-item-summary");
   const accordionSummaryClass = accordionSummary.className
   const accordionLandingItemRoots = document.getElementsByClassName(accordionSummaryClass)
@@ -26,25 +38,13 @@ test('renders the summary properly for false initial state', () => {
   expect(style.opacity).toBe("")
 });
 
-test('renders the summary properly for true initial state', () => {
-  const { getByTestId } = render(<AccordionLandingItem header="header-test" summary="summary-test" initialState={true}/>);
-  const accordionSummary = getByTestId("accordion-item-summary");
-  const accordionSummaryClass = accordionSummary.className
-  const accordionLandingItemRoots = document.getElementsByClassName(accordionSummaryClass)
-  const style = window.getComputedStyle(accordionLandingItemRoots[0])
-  expect(style.opacity).toBe("1")
-});
-
 test('expand feature on the summary' , async () => {
-  const { getByTestId } = render(<AccordionLandingItem header="header-test" summary="summary-test" initialState={false}/>);
+  const { getByTestId } = render(
+    <MediaContext.Provider value={{media: true}}>
+      <AccordionLandingItem header="header-test" summary="summary-test" initialState={false}/>
+    </MediaContext.Provider>
+  );
   expect(await getByTestId("drop-down")).toBeInTheDocument();
   fireEvent.click(getByTestId("accordion-item-wrapper"))
   expect(await getByTestId("drop-up")).toBeInTheDocument();
-})
-
-test('collapse feature on the summary' , async () => {
-  const { getByTestId } = render(<AccordionLandingItem header="header-test" summary="summary-test" initialState={true}/>);
-  expect(await getByTestId("drop-up")).toBeInTheDocument();
-  fireEvent.click(getByTestId("accordion-item-wrapper"))
-  expect(await getByTestId("drop-down")).toBeInTheDocument();
 })
